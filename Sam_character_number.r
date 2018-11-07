2415b924df4a0dfb5738744866f6a3edf3fa1cd5
Russell Garwood <russell.garwood@manchester.ac.uk>
2018-11-07 15:26:50 +0000

    Continued development of Sam's script

--- Sam_character_number.r
+++ Sam_character_number.r
@@ -1,12 +1,34 @@
 
 #To run in R source("Sam_character_number_script.r")
 #Set up file for writing
-outFile<-file("_batch_modified.tnt","a")
+outFile<-file("_batch_modified.tnt","a");
 #Read file in vector
-textLines <- readLines("_batch.nex")
+textLines <- readLines("_batch.nex");
+
+characterNumberChar <- readline(prompt="How many characters would you like?");
+characterNumberInt <- asInteger(characterNumberChar);
 #Loop through vector
+
+count = 0;
 for (currentLine in textLines)
 {
-  if (!startsWith(currentLine, ("1024"))&!startsWith(currentLine, ("Species_")))writeLines(currentLine,outFile)
+  
+  # Fix character line
+  if (startsWith(currentLine, ("1024")))
+  {
+    gsub("1024",characterNumberChar,currentLine);
+    count <-count+1;
+    sprintf("Dataset: %i", count);
+  }
+  #Fix data line
+  if (startsWith(currentLine, ("Species_")))
+  {
+    position <- regexpr(" 0", currentLine);
+    if (position<0)position <- regexpr(" 1", currentLine);
+    currentLine = substr(currentLine, 1, (position+characterNumberInt));
+  }
+
+  #Write line to new file
+  writeLines(currentLine,outFile)
 }
 close(outFile)
