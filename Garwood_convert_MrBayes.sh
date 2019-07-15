df2354f1a7aa66a44ae6a457575f77476c80280d
RussellGarwood <russell.garwood@manchester.ac.uk>
2019-07-15 09:49:47 +0100

    Updated MrBayes script

--- Garwood_convert_MrBayes.sh
+++ Garwood_convert_MrBayes.sh
@@ -1,6 +1,6 @@
 #A script by Russell Garwood that uses sed to convert data in TNT format to a format which can be run in MrBayes. 
-sed 's/[/(/g' -i ./Garwood_Dunlop_chelicerate_matrix_development_bayes.nex
-sed 's/]/)/g' -i ./Garwood_Dunlop_chelicerate_matrix_development_bayes.nex
-sed 's/nstates nogaps;/#NEXUS/g' -i ./Garwood_Dunlop_chelicerate_matrix_development_bayes.nex
-sed 's/xread/BEGIN DATA;\nDIMENSIONS NTAX=XXX NCHAR=XXX;\nFormat datatype=standard interleave=no GAP=- MISSING=? ;\n\nMATRIX\n/g' -i ./Garwood_Dunlop_chelicerate_matrix_development_bayes.nex
-sed 's/ccode [-.;/END;/g' -i ./Garwood_Dunlop_chelicerate_matrix_development_bayes.nex
+sed 's/\[/(/g' -i ./"Garwood_Dunlop_chelicerate_matrix_development - Matrix - Jul 19.txt"
+sed 's/]/)/g' -i ./"Garwood_Dunlop_chelicerate_matrix_development - Matrix - Jul 19.txt"
+sed 's/nstates nogaps;/#NEXUS/g' -i ./"Garwood_Dunlop_chelicerate_matrix_development - Matrix - Jul 19.txt"
+sed 's/xread/BEGIN DATA;\nDIMENSIONS NTAX=XXX NCHAR=XXX;\nFormat datatype=standard interleave=no GAP=- MISSING=? ;\n\nMATRIX\n/g' -i ./"Garwood_Dunlop_chelicerate_matrix_development - Matrix - Jul 19.txt"
+sed 's/ccode [-.;/END;/g' -i ./"Garwood_Dunlop_chelicerate_matrix_development - Matrix - Jul 19.txt"
