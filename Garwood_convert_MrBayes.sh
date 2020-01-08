ca27d90b320f9b48cff234f6cadf872c990588fd
RussellGarwood <russell.garwood@manchester.ac.uk>
2020-01-08 13:11:56 +0000

    Update to bayes convert script to match new ccode end

--- Garwood_convert_MrBayes.sh
+++ Garwood_convert_MrBayes.sh
@@ -3,4 +3,4 @@ sed 's/\[/(/g' -i ./"Garwood_Dunlop_chelicerate_matrix_development - Matrix - Ju
 sed 's/]/)/g' -i ./"Garwood_Dunlop_chelicerate_matrix_development - Matrix - Jul 19.txt"
 sed 's/nstates nogaps;/#NEXUS/g' -i ./"Garwood_Dunlop_chelicerate_matrix_development - Matrix - Jul 19.txt"
 sed 's/xread/BEGIN DATA;\nDIMENSIONS NTAX=XXX NCHAR=XXX;\nFormat datatype=standard interleave=no GAP=- MISSING=? ;\n\nMATRIX\n/g' -i ./"Garwood_Dunlop_chelicerate_matrix_development - Matrix - Jul 19.txt"
-sed 's/ccode [-.;/END;/g' -i ./"Garwood_Dunlop_chelicerate_matrix_development - Matrix - Jul 19.txt"
+sed 's/ccode (-.;/END;/g' -i ./"Garwood_Dunlop_chelicerate_matrix_development - Matrix - Jul 19.txt"
